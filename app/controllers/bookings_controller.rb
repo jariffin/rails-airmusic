7902cc7d4d3c015f8347eae11251313bef92fe4e
Juliana Ariffin <juliana@fastmail.com>
2020-04-22 15:12:16 +0200

    create-review

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,7 +11,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(bookings_params)
     @instrument = Instrument.find(params[:instrument_id])
-    @booking.instrument = @instrument
+    @booking.instrument_id = @instrument.id
     @booking.user_id = current_user.id
     if @booking.save
       redirect_to instrument_path(@instrument)
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,20 @@
 class ReviewsController < ApplicationController
 
   def new
-
+    @review = Review.new
   end
 
   def create
-
+    @review = Review.new(reviews_params)
+    @instrument = Instrument.find(params[:instrument_id])
+    @review.user_id = current_user.id
+    @review.instrument_id = @instrument.id
+    if @review.save
+      flash[:notice] = "Thank you for your review!"
+      redirect_to instrument_path(@instrument)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -15,7 +24,7 @@ class ReviewsController < ApplicationController
   private
 
   def reviews_params
-
+    params.require(:review).permit(:content)
   end
 
 end
