2eb5fe94d1eecbbf279102ace27338e0befef7c4
Juliana Ariffin <juliana@fastmail.com>
2020-04-24 11:34:36 +0200

    search working

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,4 +3,24 @@ class Instrument < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+
+  # with s, that means Instrument has_many reviews
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :description ],
+    associated_against: {
+      reviews: [ :content ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name_and_description,
+  #   against: [ :name, :description ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
